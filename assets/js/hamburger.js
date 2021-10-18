3a6c367f9c85655e4495cc0718bcc56f35a2f1d0
James Atkinson <89410673+Archangel767@users.noreply.github.com>
2021-10-17 23:22:47 -0600

    Add files via upload

---
+++ assets/js/hamburger.js
@@ -0,0 +1,20 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+const navright = document.querySelector('.nav-right');
+const navLinks = Array.from(document.querySelectorAll('.nav-link'));
+
+menuToggle.addEventListener('click', ()=>{
+  menuToggle.classList.toggle('active');
+  navright.classList.toggle('active');
+  nav.classList.toggle('active');
+
+
+// Remove active class to restart the delayed effect
+    navright.classList.remove('active');
+
+    if(menuToggle.classList.contains('active')){
+    setTimeout(()=>{
+      navright.classList.add('active');
+    },100)
+  }
+})
